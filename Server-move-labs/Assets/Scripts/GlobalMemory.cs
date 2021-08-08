c47549e6c6652c8eb56d8cb60902c502ccbf519f
Calico <ireneecs@qq.com>
2021-08-07 19:37:15 -0700

    3 techniques
    - direct drag
    - hold and tap
    - throw and catch

---
+++ Server-move-labs/Assets/Scripts/GlobalMemory.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static PublicLabFactors;
+
+
+public class GlobalMemory : MonoBehaviour
+{
+    public static GlobalMemory Instance;
+
+    public ServerController server;
+    public demoDataMemory demoData;
+
+    [HideInInspector]
+    public string serverip;
+    [HideInInspector]
+    public ServerCommand curServerCommand;
+    [HideInInspector]
+    public LabScene curServerScene, curClientScene;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
